923dee57c9c0d29c14c25ce7d2aab0f3239ae45d
sapphire <info@sapphire.sh>
2022-07-29 00:21:36 +0900

    #1342

--- src/1342.js
+++ src/1342.js
@@ -2,16 +2,16 @@
  * @param {number} num
  * @return {number}
  */
-var numberOfSteps  = function(num) {
-    let count = 0;
-    while(num > 0) {
+ var numberOfSteps = function(num) {
+    let c = 0;
+    while(num !== 0) {
         if(num % 2 === 0) {
             num /= 2;
         }
         else {
             num -= 1;
         }
-        ++count;
+        c+= 1;
     }
-    return count;
+    return c;
 };
