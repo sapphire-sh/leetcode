f54c7fee807315aadfd8f3d7cd3ce053d00fb846
sapphire <info@sapphire.sh>
2022-08-07 02:43:17 +0900

    #0226

--- src/0226.js
+++ src/0226.js
@@ -1,21 +1,20 @@
 /**
  * Definition for a binary tree node.
- * function TreeNode(val) {
- *     this.val = val;
- *     this.left = this.right = null;
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
  * }
  */
 /**
  * @param {TreeNode} root
  * @return {TreeNode}
  */
-var invertTree = function(root) {
+ var invertTree = function(root) {
     if(root === null) {
-        return root;
+        return null;
     }
-    const t = root.left;
-    root.left = root.right;
-    root.right = t;
+    [root.left, root.right] = [root.right, root.left];
     invertTree(root.left);
     invertTree(root.right);
     return root;
